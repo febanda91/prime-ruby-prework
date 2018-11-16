beae366970a33bd82127efa4378a41d3bb0453ab
Francisco Ezeqiel Banda <febanda91@gmail.com>
2018-11-16 21:28:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(integer)
-  if integer > 0
-  (2..integer-1).each {|i| return false if integer <= 1 || integer % i == 0}
+if integer > 0
+(2..integer-1).each {|i| return false if integer <= 1 || integer % i == 0}
 else
   return true
  end
